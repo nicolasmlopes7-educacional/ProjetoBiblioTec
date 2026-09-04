6312e1a245ffbeebdca8f348af52f57c8012f957
NICOLAS MARCELINO LOPES <54815787867@info.local>
2026-09-04 17:26:57 -0300

    :sparkles: feat: Página de login

---
+++ login/login.js
@@ -0,0 +1,9 @@
+
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+btnSenha.addEventListener("click", function() {
+    
+    alert("Cliquei no botão!");
+
+})
