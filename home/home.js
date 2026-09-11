cb607d13fbed95546cc79b1e208b836dd4a1086e
NICOLAS MARCELINO LOPES <54815787867@info.local>
2026-09-11 17:26:35 -0300

    :iphone: feat: Responsividade para 500px e funções do JavaScript

---
+++ home/home.js
@@ -0,0 +1,19 @@
+
+const menu = document.getElementById("menu"); // pegando elemento pelo ID
+
+// estamos pegando o elemento de forma genérica - pode ser classe (precisa trazer o ponto) ou pode ser ID (precisa trazer a hashtag)
+const navMenu = document.querySelector(".navegacao");
+
+menu.addEventListener("click", function () {
+
+    if (navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo";
+    }
+
+    else {
+        navMenu.className = "navegacao";
+    }
+})
+
+// opção com arrow function -> função lambda
+// menu.addEventListener("click", () => {})
